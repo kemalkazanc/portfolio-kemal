8f670b8ed711765372a28b16e8eac06a7ed22103
Kemal Kazanc <Keemalkazanc@gmail.com>
2024-05-17 23:41:26 +0200

    update react + composant

---
+++ src/components/ProjectSection.jsx
@@ -0,0 +1,11 @@
+import React from "@astrojs/react";
+
+const ProjectSection = () => {
+  return (
+    <div>
+      <h1>Test</h1>
+    </div>
+  );
+};
+
+export default ProjectSection;
---
+++ src/lib/config.js
@@ -0,0 +1,25 @@
+import { SiGithub, SiLinkedin, SiTwitter } from "react-icons/si";
+
+export const GITHUB_USERNAME = "kemalkazanc";
+
+export const EMAIL = "keemalkazanc@gmail.com";
+
+export const FULL_NAME = "Kemal Kazanc";
+
+export const SOCIAL_NETWORKS = [
+  {
+    url: "https://twitter.com/KemalKazanc9",
+    name: "Twitter",
+    icon: <SiTwitter />,
+  },
+  {
+    url: "https://www.linkedin.com/in/kemal-kazanc-a6611a22b/",
+    name: "Linkedin",
+    icon: <SiLinkedin />,
+  },
+  {
+    url: "https://github.com/kemalkazanc",
+    name: "Github",
+    icon: <SiGithub />,
+  },
+];
