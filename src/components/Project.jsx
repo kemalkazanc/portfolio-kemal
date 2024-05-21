686daedfe4715175391a0954a88b901e1a2def2a
Kemal Kazanc <Keemalkazanc@gmail.com>
2024-05-21 16:13:13 +0200

    update du style de la page bloc

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -3,25 +3,32 @@ import projectsData from "../data/projects.json";
 
 const Project = () => {
   return (
-    <section className="mt-20">
-      <h2 className="text-4xl font-bold my-6">Mes Projets</h2>
-      <div className="max-w-83rem mx-auto max-w-prose mx-auto">
+    <section className="">
+      <div className="mt-12 mb-12 border-b flex border-gray-800 wrapper justify-between pb-8">
+        <h2 className="flex justify-center text-4xl font-bold ">Mes Projets</h2>
+        <p className=" flex text-xl items-end">
+          Parcourez mes réalisations et mes articles{" "}
+        </p>
+      </div>
+      <div className="max-w-83rem max-w-prose mx-auto">
         {projectsData.map((project, index) => (
-          <div key={project.id}>
+          <div className="mt-12" key={project.id}>
             <div className="flex items-center">
               <img
-                className="w-48 h-32 mr-4"
+                className="w-60 h-44 mr-4 rounded-md drop-shadow-2xl"
                 src={project.image}
                 alt={project.name}
               />
               <div>
-                <h3 className="text-xl font-semibold mb-4">{project.name}</h3>
+                <h3 className="text-3xl font-semibold mb-4 ">{project.name}</h3>
                 <p className="text-gray-600">{project.description}</p>
               </div>
             </div>
             {/* Ligne de séparation entre les projets */}
             {index !== projectsData.length - 1 && (
-              <hr className="my-4 w-full border-gray-300" />
+              <div className="my-4">
+                <hr className="w-full border-gray-300 mb-10 mt-10" />
+              </div>
             )}
           </div>
         ))}
@@ -31,32 +38,3 @@ const Project = () => {
 };
 
 export default Project;
-
-{
-  /* <section className="flex justify-center wrapper ">
-      <div className="">
-        <h2 className="text-3xl font-bold mb-6">Mes Projets</h2>
-        <div className="grid grid-cols-1 gap-6 content">
-          {projectsData.map((project, index) => (
-            <div className="content" key={project.id}>
-              <div className="flex items-center content">
-                <img
-                  className="w-48 h-32 mr-4"
-                  src={project.image}
-                  alt={project.name}
-                />
-                <div>
-                  <h3 className="text-xl font-semibold">{project.name}</h3>
-                  <p className="text-gray-600">{project.description}</p>
-                </div>
-              </div>
-              {/* Ligne de séparation entre les projets */
-}
-//           {index !== projectsData.length - 1 && (
-//             <hr className="my-4 w-full border-gray-300" />
-//           )}
-//         </div>
-//       ))}
-//     </div>
-//   </div>
-// </section> */}
